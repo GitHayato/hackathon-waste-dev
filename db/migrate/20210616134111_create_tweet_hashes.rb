bb879da7055fe72f899d12e7fd6e1af1fafb189b
GitHayato <gabayt0_0@icloud.com>
2021-06-17 10:39:53 +0900

    rails db:migrate

--- db/migrate/20210616134111_create_tweet_hashes.rb
+++ db/migrate/20210616134111_create_tweet_hashes.rb
@@ -1,7 +1,8 @@
 class CreateTweetHashes < ActiveRecord::Migration[6.0]
   def change
     create_table :tweet_hashes do |t|
-
+      t.string      :tweet_hash, null: false
+      t.references  :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_054548) do
+ActiveRecord::Schema.define(version: 2021_06_16_134111) do
+
+  create_table "tweet_hashes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "tweet_hash", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweet_hashes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "uid"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_06_14_054548) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tweet_hashes", "users"
 end
