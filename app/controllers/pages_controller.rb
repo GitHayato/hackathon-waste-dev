68575aa39fa7d66fc1fcb6b728e08bfb5f3d3412
GitHayato <gabayt0_0@icloud.com>
2021-06-19 21:11:18 +0900

    公式アプリのツイート取得（バックエンド）

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,17 +3,26 @@ class PagesController < ApplicationController
   include SessionsHelper
   def index
     if logged_in?
-      client = Authorization.init()
-      @data = client.search("#今日の積み上げ", result_type: "recent", from: "#{current_user.nickname}").collect do |tweet|
-        {
-          "image": "#{tweet.user.profile_image_url.to_s.sub('http', 'https')}",
-          "name": "#{tweet.user.name}",
-          "text": "#{tweet.full_text}",
-          "tweet_link": "#{tweet.uri}",
-          "create": "#{tweet.created_at.strftime("%H:%M・%Y/%m/%d")}",
-          "screen_name": "#{tweet.user.screen_name}"
-        }
-      end
+      @data = data(current_user.nickname)
+    else
+      @official = data("tukikawaoshioki")
     end
+  end
+
+  private
+
+  def data(user)
+    client = Authorization.init()
+    data = client.search("#今日の積み上げ", result_type: "recent", from: user).collect do |tweet|
+      {
+        "image": "#{tweet.user.profile_image_url.to_s.sub('http', 'https')}",
+        "name": "#{tweet.user.name}",
+        "text": "#{tweet.full_text}",
+        "tweet_link": "#{tweet.uri}",
+        "create": "#{tweet.created_at.strftime("%H:%M・%Y/%m/%d")}",
+        "screen_name": "#{tweet.user.screen_name}"
+      }
+    end
+    return data
   end
 end
