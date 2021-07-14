0bcdeb92f5c406686f5d34ca073c2055a4021bd6
GitHayato <gabayt0_0@icloud.com>
2021-07-14 15:18:29 +0900

    TweetHashの型変更

--- db/migrate/20210623003838_change_data_title_to_tweet_hash.rb
+++ db/migrate/20210623003838_change_data_title_to_tweet_hash.rb
@@ -1,7 +1,7 @@
 class ChangeDataTitleToTweetHash < ActiveRecord::Migration[6.0]
   def change
-    add_column :tweet_hashes, :end_time, :float
-    change_column :tweet_hashes, :start_time, :float
+    add_column :tweet_hashes, :end_time, :string
+    change_column :tweet_hashes, :start_time, :string
   end
 
 end
